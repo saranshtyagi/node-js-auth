8913855138437f9cda026b51fbb3bde33c193af9
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-07-04 19:17:55 +0530

    implemented login controller logic

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const User = require("../models/User");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const registerUser = async(req, res) => {
     try {
@@ -46,7 +48,39 @@ const registerUser = async(req, res) => {
 
 const loginUser = async(req, res) => {
     try {
-        
+        const { username, email } = req.body; 
+        const user = await User.findOne({ username }); 
+
+        if(!user) {
+            res.status(400).json({
+                success: false, 
+                message: "User doesn't exist!"
+            });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password); 
+
+        if(!isPasswordMatch) {
+            res.status(400).json({
+                success: false, 
+                message: "Incorrect username or password"
+            });
+        }
+
+        const accessToken = jwt.sign({
+            userId: user._id, 
+            username: user.username, 
+            role: user.role
+        }, process.env.JWT_SECRET_KEY, {
+            expiresIn: '15m'
+        });
+
+        res.status(200).json({
+            success: true, 
+            message: 'Logged in successfully', 
+            accessToken
+        });
+
     } catch (error) {
         res.status(500).json({
             success: false, 
