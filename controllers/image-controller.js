2454e9bf222e2b509be8c184b38ed31db9433b8a
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-07-10 00:15:01 +0530

    fix: enhance image upload functionality and error handling

--- controllers/image-controller.js
+++ controllers/image-controller.js
@@ -1,3 +1,6 @@
+const Image = require('../models/Image'); 
+const { uploadToCloudinary } = require('../helpers/cloudinaryHelper');
+
 const uploadImage = async(req, res) => {
     try {
         if(!req.file) {
@@ -6,7 +9,26 @@ const uploadImage = async(req, res) => {
                 message: 'File is missing, file is required!'
             });
         }
-        
+
+        const { url, publicId } = await uploadToCloudinary(req.file.path);
+
+        const newlyUploadedImage = await Image.create({
+            url,
+            publicId, 
+            uploadedBy: req.userInfo.userId
+        });
+
+        if(!newlyUploadedImage) {
+            res.status(400).json({
+                success: false, 
+                message: "Could not upload image!"
+            });
+        }
+        res.status(201).json({
+            success: true, 
+            message: 'Image uploaded successfully!', 
+            image: newlyUploadedImage
+        });
     } catch (error) {
         res.status(500).json({
             success: false, 
@@ -14,3 +36,5 @@ const uploadImage = async(req, res) => {
         });
     }
 }
+
+module.exports = uploadImage;
