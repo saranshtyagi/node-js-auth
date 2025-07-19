7764e9baacd2c6db7f12b30cec8d70a632d7b20f
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-07-19 14:46:34 +0530

    feat: added route for change password functionality

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,8 +1,11 @@
 const express = require('express'); 
-const { loginUser, registerUser } = require('../controllers/auth-controller');
+const { loginUser, registerUser, changePassword } = require('../controllers/auth-controller');
+const authMiddleware = require('../middleware/auth-middleware');
 const router = express.Router(); 
+
 
 router.post('/login', loginUser); 
 router.post('/register', registerUser); 
+router.post('/change-password', authMiddleware,changePassword);
 
 module.exports = router;
